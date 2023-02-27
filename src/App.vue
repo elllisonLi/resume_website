20941ad358dce4e1e76e67e04d643656775e8b29
elllisonLi <yul054@ucsd.edu>
2023-02-27 09:46:37 -0800

    :tada: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>个人简历</h1>
+  <el-button>123</el-button>
+  <el-icon>
+    <i-ep-chat-square></i-ep-chat-square>
+  </el-icon>
+</template>
+
+<script setup>
+
+</script>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
